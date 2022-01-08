dcaa69d74dd82a10d6edc73966ddf0705575d08f
Gundul <sampah@mailbox.org>
2022-01-08 11:40:30 +0100

    first vertical collision

--- common/raycast.c
+++ common/raycast.c
@@ -6,7 +6,7 @@
 /*   By: graja <graja@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 12:15:47 by graja             #+#    #+#             */
-/*   Updated: 2022/01/08 08:44:53 by graja            ###   ########.fr       */
+/*   Updated: 2022/01/08 11:39:37 by graja            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,36 @@ void	ft_firstHitHoricental(t_data *d, float alpha)
 	float	diffX;
 
 	if (alpha >= 270.0 || alpha <= 90.0)
-	{
 		diffY = d->py - floorf(d->py);
-		diffX = diffY / tanf(ft_deg2rad(alpha));
-		diffX = (d->px - diffX) * (float)(d->win_x / d->mapx);
-		diffY = (d->py - diffY) * (float)(d->win_y / d->mapy);
-		ft_draw_hit(d, diffX, diffY, 3.0, ft_make_trgb(0, 51, 0, 255));
-	}
 	else
-	{
 		diffY = d->py - ceilf(d->py);
-		diffX = diffY / tanf(ft_deg2rad(alpha));
-		diffX = (d->px - diffX) * (float)(d->win_x / d->mapx);
-		diffY = (d->py - diffY) * (float)(d->win_y / d->mapy);
-		ft_draw_hit(d, diffX, diffY, 3.0, ft_make_trgb(0, 51, 0, 255));
-	}
+	diffX = diffY / tanf(ft_deg2rad(alpha));
+	diffX = (d->px - diffX) * (float)(d->win_x / d->mapx);
+	diffY = (d->py - diffY) * (float)(d->win_y / d->mapy);
+	ft_draw_circle(d, diffX, diffY, 3.0, ft_make_trgb(0, 39, 78, 19));
+}
+
+
+/* 
+ * alpha must be not equal to 0 or 180.
+ * in this case diffY would be infinite and program segfaults
+ */
+static
+void	ft_firstHitVertical(t_data *d, float alpha)
+{
+	float	diffY;
+	float	diffX;
+
+	if (alpha >= 0.0 && alpha <= 180.0)
+		diffX = ceilf(d->px) - d->px;
+	else
+		diffX = floorf(d->px) - d->px;
+	diffY = diffX * tanf(ft_deg2rad(alpha));
+	diffX = (d->px + diffX) * (float)(d->win_x / d->mapx);
+	diffY = (d->py + diffY) * (float)(d->win_y / d->mapy);
+	if (diffX > 0.0 && diffX < d->win_x && diffY > 0.0 && diffY < d->win_y)
+		ft_draw_circle(d, diffX, diffY, 3.0, ft_make_trgb(0, 102, 0, 0));
+
 }
 
 void	ft_castRay(t_data *d, size_t x, size_t y)
@@ -50,4 +65,5 @@ void	ft_castRay(t_data *d, size_t x, size_t y)
 	ft_draw_angeled(d, px, py, alpha, len);
 	printf("%7f \n", alpha);
 	ft_firstHitHoricental(d, alpha);
+	ft_firstHitVertical(d, alpha);
 }
